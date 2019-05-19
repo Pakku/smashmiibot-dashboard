f0bf73bf172ec24ea32708d13d7bff1dbf04afb5
Pakku <quentyaiden@gmail.com>
2019-05-19 16:57:12 +0200

    Base con vue router.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,19 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
-window.Vue = require('vue');
+// Route information for Vue Router
+import Routes from '@/js/routes.js';
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+// Component File
+import App from '@/js/views/App';
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(Vuetify);
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router: Routes,
+    render: h => h(App),
 });
+
+export default app;
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,14 @@
 const mix = require('laravel-mix');
 
+mix.webpackConfig({
+	resolve: {
+		extensions: ['.js', '.vue'],
+		alias: {
+			'@': __dirname + '/resources'
+		}
+	}
+});
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +21,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .version();
